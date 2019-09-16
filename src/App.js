5f16db5736cec5417dfa246a62fe642b6ad40cab
Piyush Joshi <piyush.joshi@TILLT06735.local>
2019-09-16 15:33:24 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import FacebookLogin from 'react-facebook-login';
+
+import GoogleLogin from 'react-google-login';
+
+class App extends Component {
+
+  render() {
+
+   
+
+    const responseGoogle = (response) => {
+      console.log(response);
+    }
+
+    return (
+      <div className="App">
+        <h1>LOGIN WITH GOOGLE</h1>
+
+      <br />
+      <br />
+
+
+      <GoogleLogin
+        clientId="518518358068-4lspganv78urkgj7te6s59a2nkaa2a46.apps.googleusercontent.com" 
+        buttonText="LOGIN WITH GOOGLE"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+      />
+
+      </div>
+    );
+  }
 }
 
 export default App;
